8b47e01968f1a7b47e0984b7fb8a7fca232385bb
mark miranda <notmarkmiranda@gmail.com>
2017-08-04 12:16:06 -0600

    update typo

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,4 @@
 if Rails.env == 'production'
   uri = URI.parse(ENV['REDISTOGO_URL'])
-  Resque.redis = Redis.new(:host => uri.hoset, :port => uri.port, :password => uri.password)
+  Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 end
